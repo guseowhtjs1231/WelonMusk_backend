8e0d8714f4987ab2225c8e7076aad0fb5dfed2ee
evergreen-david <leesangrok1118@gmail.com>
2019-12-26 08:31:53 -0500

    Add common function for password verification

---
+++ user/common.py
@@ -0,0 +1,12 @@
+from django.views import View
+
+class Common(View):
+    
+    def checkPassword(self, password):
+        
+        if len(password) < 8:
+            return 'SHORT_PASSWORD'
+        elif not any(c.isupper() for c in password):
+            return 'NO_CAPITAL_LETTER_PASSWORD'
+        else:
+            return None
