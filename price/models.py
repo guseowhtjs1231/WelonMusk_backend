aa765cc71257e45f693f0da56c2f4513900fd004
evergreen-david <leesangrok1118@gmail.com>
2019-12-27 05:39:15 -0500

    Fix the decimal field

--- price/models.py
+++ price/models.py
@@ -14,9 +14,9 @@ class CarTypes(models.Model):
         db_table = 'car_types'
 
 class CarTypePrices(models.Model):
-    model_id = models.ForeignKey('CarModels', related_name='car_models', on_delete= models.SET_NULL, null=True)
-    type_id = models.ForeignKey('CarTypes', related_name='car_types', on_delete= models.SET_NULL, null=True)
-    basic_price = models.DecimalField(max_digits = 10, decimal_places = 8)
+    model = models.ForeignKey('CarModels', related_name='car_models', on_delete= models.SET_NULL, null=True)
+    type = models.ForeignKey('CarTypes', related_name='car_types', on_delete= models.SET_NULL, null=True)
+    basic_price = models.DecimalField(max_digits = 19, decimal_places = 2)
 
     class Meta:
         db_table = 'car_type_prices'
