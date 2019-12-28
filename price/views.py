5714ffe3c9210b9ac46735b818d7ffe6579aa0c6
evergreen-david <leesangrok1118@gmail.com>
2019-12-28 02:24:47 -0500

    add a price api based on car models

--- price/views.py
+++ price/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .models      import CarModels, CarTypes, CarTypePrices
+from django.views import View
+from django.http  import JsonResponse
 
-# Create your views here.
+
+class PriceView(View):
+
+    def get(self, request, model_id):
+        try:
+            cars = CarModels.objects.prefetch_related('car_models').get(id=model_id)
+            type_list = list(cars.car_models.all().values('type_id','basic_price'))
+
+            for i in range(0, len(type_list)):
+                car_type = CarTypes.objects.get(id=type_list[i]['type_id'])
+                type_list[i]['model_type'] = car_type.model_type
+                del type_list[i]['type_id']
+
+            return JsonResponse({'message':'SUCCESS','data':type_list}, status=200)
+
+        except CarModels.DoesNotExist:
+            return JsonResponse({'message':'INVALID_MODEL'}, status = 400)
