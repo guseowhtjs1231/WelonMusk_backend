57b46d552e28754f165057a8e5e8be195603de89
evergreen-david <leesangrok1118@gmail.com>
2019-12-29 22:06:34 -0500

    update api naming

--- price/urls.py
+++ price/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views      import PriceView
 
 urlpatterns = [
-    path('/getModelPrice/<int:model_id>', PriceView.as_view()),
+    path('/model/<int:model_id>', PriceView.as_view()),
 ]
