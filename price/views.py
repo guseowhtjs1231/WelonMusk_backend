d09502178cc13b1b72b4598c12058e1d48aa7145
evergreen-david <leesangrok1118@gmail.com>
2019-12-30 08:30:40 -0500

    more refactoring

--- price/views.py
+++ price/views.py
@@ -8,12 +8,13 @@ class PriceView(View):
     def get(self, request, model_id):
         try:
             cars = CarModels.objects.prefetch_related('car_models').get(id=model_id)
-            type_list = list(cars.car_models.all().values('type_id','basic_price'))
 
-            for car in type_list:
-                car_type = CarTypes.objects.get(id=car['type_id'])
-                car['model_type'] = car_type.model_type
-                del car['type_id']
+            type_list = [
+            {
+                'model_name' : cars.model_name ,
+                'basic_price' : car['basic_price'],
+                'model_type' : CarTypes.objects.get(id=car['type_id']).model_type,
+            } for car in list(cars.car_models.all().values())]
 
             return JsonResponse({'message':'SUCCESS','data':type_list}, status=200)
 
