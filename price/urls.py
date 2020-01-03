ad12543812eabf10e3830f091fe0b0ff4d2427ca
evergreen-david <leesangrok1118@gmail.com>
2020-01-02 23:32:45 -0500

    Add the seat price view

--- price/urls.py
+++ price/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views      import PriceView, ColorPriceView
+from .views      import CarSeatPrice
 
 urlpatterns = [
     path('/model/<int:model_id>', PriceView.as_view()),
     path('/color/<int:model_id>', ColorPriceView.as_view()),
+    path('/seat/<int:model_id>', CarSeatPrice.as_view()),
 ]
--- price/views.py
+++ price/views.py
@@ -1,6 +1,7 @@
 from django.views import View
 from django.http  import JsonResponse
 from .models      import CarModels, CarTypes, CarTypePrices, CarColors
+from .models      import CarSeats
 
 class PriceView(View):
     def get(self, request, model_id):
@@ -8,9 +9,9 @@ class PriceView(View):
             cars = CarModels.objects.prefetch_related('car_models').get(id=model_id)
             type_list = [
             {
-                'model_name' : cars.model_name ,
+                'model_name'  : cars.model_name ,
                 'basic_price' : car['basic_price'],
-                'model_type' : CarTypes.objects.get(id=car['type_id']).model_type,
+                'model_type'  : CarTypes.objects.get(id=car['type_id']).model_type,
             } for car in list(cars.car_models.all().values())]
 
             return JsonResponse({'message':'SUCCESS','data':type_list}, status=200)
@@ -25,11 +26,11 @@ class ColorPriceView(View):
 
             color_list = [
             {
-                'model_name' : cars.model_name,
-                'color_id' : CarColors.objects.get(id=car['color_id']).id,
+                'model_name'  : cars.model_name,
+                'color_id'    : CarColors.objects.get(id=car['color_id']).id,
                 'color_price' : car['color_price'],
-                'color_name' : CarColors.objects.get(id=car['color_id']).color_name,
-                'img_url' : CarColors.objects.get(id=car['color_id']).img_url,
+                'color_name'  : CarColors.objects.get(id=car['color_id']).color_name,
+                'img_url'     : CarColors.objects.get(id=car['color_id']).img_url,
             } for car in list(cars.carcolorprices_set.all().values())]
 
             return JsonResponse({'message':'SUCCESS','data':color_list}, status=200)
@@ -37,3 +38,20 @@ class ColorPriceView(View):
         except CarModels.DoesNotExist:
             return JsonResponse({'message':'INVALID_MODEL'}, status = 400)
 
+class CarSeatPrice(View):
+
+    def get(self, request, model_id):
+        try:
+            cars = CarModels.objects.prefetch_related('carseatprices_set').get(id=model_id)
+            type_list = [
+            {
+                'model_name' : cars.model_name ,
+                'seat_id'    : car['seat_id'],
+                'seat_name'  : CarSeats.objects.get(id=car['seat_id']).seat_name,
+                'seat_price' : round(car['seat_price'], 0)
+            } for car in list(cars.carseatprices_set.all().values())]
+
+            return JsonResponse({'message':'SUCCESS','data':type_list}, status=200)
+
+        except CarModels.DoesNotExist:
+            return JsonResponse({'message':'INVALID_MODEL'}, status = 400)
